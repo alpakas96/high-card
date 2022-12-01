f99b06caafc19f83a8abebf540c213d76b42c34f
alpakas96 <jhurleyfilm@gmail.com>
2022-12-01 13:17:49 -0500

    creating deck of cards now working

---
+++ app.js
@@ -0,0 +1,50 @@
+let suits = ["clubs", "spades", "hearts", "diamonds"]
+let colors = ["black", "red"]
+let shuffledDeck = []
+let deck = [];
+
+
+class card {
+    constructor(number, suit, color) {
+        this.number = number; 
+        this.suit = suit; 
+        this.color = color; 
+    }
+}
+
+//WORKING -- RETURNS AN ARRAY OF CARDS BASED ON THE CARD CLASS
+function newDeck ( ) {
+
+        //THIS FOR LOOP CREATES THE BLACK CARDS: CLUBS AND SPADES
+        //the j loop controls the suit
+        for (j = 0; j <= 1; j++) {
+            //the i loop controls the number (face value)
+            for (i = 0; i <=13; i++) {
+                const newCard = new card (i, suits[j], "black" ); 
+                deck.push(newCard); 
+            }
+        }
+
+        //THIS FOR LOOP CREATES THE RED CARDS: HEARTS AND DIAMONDS
+        //the j loop controls the suit
+        for (j = 2; j <= 3; j++) {
+            //the i loop controls the number (face value)
+            for (i = 0; i <=13; i++) {
+                const newCard = new card (i, suits[j], "red" ); 
+                deck.push(newCard); 
+            }
+        }
+    return deck;
+}
+
+
+    // this function randomizes the deck array created by the newDeck function
+    function shuffle(newDeck) {
+        for (i = 0; i <= newDeck.length; i++) {
+            let x = Math.random( )*newDeck.length; 
+            shuffledDeck.splice(x, 0, newDeck[x]);
+            newDeck.splice(x, 1); 
+        } 
+    }
+
+    console.log(typeof deck)
